5505766083570bda0c40fb9e83edebd9daddfe1c
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-18 21:43:54 +0600

    UI implemented

--- XCAChatGPT/ContentView.swift
+++ XCAChatGPT/ContentView.swift
@@ -5,40 +5,126 @@
 //  Created by Joynal Abedin on 18/3/23.
 //
 
+/*
+ .onAppear {
+ Task {
+ let api = ChatGPTAPI(apiKey: "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")
+ do {
+ //                    let stream = try await api.sendMessageStream(text: "What is James Bond?")
+ //                    for try await line in stream {
+ //                        print(line)
+ //                    }
+ let text = try await api.sendMessage("Tell me about c program?")
+ print(text)
+ let text1 = try await api.sendMessage("write c program how can i sum two number?")
+ print(text1)
+ 
+ }catch {
+ print(error.localizedDescription)
+ }
+ }
+ }
+ */
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.colorScheme) private var colorScheme
+    @StateObject var vm = ViewModel(api: ChatGPTAPI(apiKey: "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"))
+    @FocusState var isTextFieldFocused: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-        .onAppear {
-            Task {
-                let api = ChatGPTAPI(apiKey: "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")
-                do {
-//                    let stream = try await api.sendMessageStream(text: "What is James Bond?")
-//                    for try await line in stream {
-//                        print(line)
-//                    }
-                    let text = try await api.sendMessage("Tell me about c program?")
-                    print(text)
-                    let text1 = try await api.sendMessage("write c program how can i sum two number?")
-                    print(text1)
-                        
-                }catch {
-                    print(error.localizedDescription)
+        chatListView
+            .navigationTitle("Ask Me Anything!")
+    }
+    
+    var chatListView: some View {
+        ScrollViewReader { proxy in
+            VStack(spacing: 0) {
+                ScrollView {
+                    LazyVStack(spacing: 0) {
+                        ForEach(vm.messages) { message in
+                            MessageRowView(message: message) { message in
+                                Task { @MainActor in
+                                    await vm.retry(message: message)
+                                }
+                            }
+                        }
+                    }
+                    .onTapGesture {
+                        isTextFieldFocused = false
+                    }
                 }
+                Divider()
+                bottomView(image: "profile", proxy: proxy)
+                Spacer()
+            }
+            .onChange(of: vm.messages.last?.responseText) { _ in
+                scrollToBottom(proxy: proxy)
             }
         }
+        .background(colorScheme == .light ? .white : Color(red: 52/255, green: 53/255, blue: 65/255, opacity: 0.5))
+    }
+    
+    func bottomView(image: String, proxy: ScrollViewProxy) -> some View {
+        HStack(alignment: .top, spacing: 8) {
+            if image.hasPrefix("http"), let url = URL(string: image) {
+                AsyncImage(url: url) { image in
+                    image
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                } placeholder: {
+                    ProgressView()
+                }
+            }
+            else {
+                Image(image)
+                    .resizable()
+                    .frame(width: 30, height: 30)
+            }
+            
+            TextField("Send Mesaage", text: $vm.inputMessage, axis: .vertical)
+                .textFieldStyle(.roundedBorder)
+                .focused($isTextFieldFocused)
+                .disabled(vm.isInteractingWithChatGPT)
+            
+            if vm.isInteractingWithChatGPT {
+                DotLoadingView()
+                    .frame(width: 60, height: 30)
+            }else{
+                Button {
+                    Task { @MainActor in
+                        isTextFieldFocused = false
+                        scrollToBottom(proxy: proxy)
+                        await vm.sendTapped()
+                    }
+                } label: {
+                    Image(systemName: "paperplane.circle.fill")
+                        .rotationEffect(.degrees(45))
+                        .font(.system(size: 30))
+                }
+                .disabled(vm.inputMessage.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
+                
+                
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.top, 12)
+    }
+    
+    private func scrollToBottom(proxy: ScrollViewProxy) {
+        guard let id = vm.messages.last?.id else {
+            return
+        }
+        proxy.scrollTo(id, anchor: .bottomTrailing)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationStack {
+            ContentView()
+        }
     }
 }
---
+++ XCAChatGPT/MessageRowView.swift
@@ -0,0 +1,95 @@
+//
+//  MessageRowView.swift
+//  XCAChatGPT
+//
+//  Created by Joynal Abedin on 18/3/23.
+//
+
+import SwiftUI
+
+struct MessageRowView: View {
+    
+    @Environment(\.colorScheme) private var colorScheme
+    let message: MessageRow
+    let retryCallback: (MessageRow) -> Void
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            messageRow(text: message.sendText, image: message.sendImage, bgColor: colorScheme == .light ? .white : Color(red: 52/255, green: 53/255, blue: 65/255, opacity: 0.5))
+            
+            if let text = message.responseText {
+                Divider()
+                messageRow(text: text, image: message.responseImage, bgColor: colorScheme == .light ? .gray.opacity(0.1) : Color(red: 52/255, green: 53/255, blue: 65/255, opacity: 1), responseError: message.responseError, showDotLoading: message.isInteractWithChatGPT)
+                Divider()
+            }
+        }
+    }
+    
+    func messageRow(text: String, image: String, bgColor: Color, responseError: String? = nil, showDotLoading: Bool = false) -> some View {
+        HStack(alignment: .top, spacing: 24) {
+            if image.hasPrefix("http"), let url = URL(string: image) {
+                AsyncImage(url: url) { image in
+                    image
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                } placeholder: {
+                    ProgressView()
+                }
+            }else {
+                Image(image)
+                    .resizable()
+                    .frame(width: 25, height: 25)
+            }
+            
+            VStack(alignment: .leading) {
+                if !text.isEmpty {
+                    Text(text)
+                        .multilineTextAlignment(.leading)
+                        .textSelection(.enabled)
+                }
+                
+                if let error = responseError {
+                    Text("Error: \(error)")
+                        .foregroundColor(.red)
+                        .multilineTextAlignment(.leading)
+                    Button("Regenerate response") {
+                        retryCallback(message)
+                    }
+                    .foregroundColor(.accentColor)
+                    .padding(.top)
+                }
+                
+                if showDotLoading {
+                    DotLoadingView()
+                        .frame(width: 60, height: 30)
+                }
+            }
+        }
+        .padding(16)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(bgColor)
+    }
+}
+
+struct MessageRowView_Previews: PreviewProvider {
+    
+    static let message = MessageRow(isInteractWithChatGPT: true, sendImage: "profile", sendText: "What is SwiftUI?", responseImage: "openai", responseText: "SwiftUI is a user interface that allows developers to design and develop user interface for iOS, macOS, watchOS and tvOS applications using swift, a programming language developed by Apple Inc.")
+    
+    static let message2 = MessageRow(isInteractWithChatGPT: false, sendImage: "profile", sendText: "What is SwiftUI?", responseImage: "openai", responseText: "", responseError: "ChatGPT is currently not available")
+    
+    static var previews: some View {
+        NavigationStack {
+            ScrollView {
+                MessageRowView(message: message, retryCallback: { messageRow in
+                    //
+                })
+                
+                MessageRowView(message: message2, retryCallback: { messageRow in
+                    //
+                })
+            }
+            .frame(width: 400)
+            .previewLayout(.sizeThatFits)
+        }
+    }
+}
